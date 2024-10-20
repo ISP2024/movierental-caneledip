e1aa93290632050a2dc2e520773a83d7c129074d
Canele <winwin6d@hotmail.com>
2024-10-20 20:14:46 +0700

    add new attribute to Movie class

--- movie.py
+++ movie.py
@@ -1,17 +1,25 @@
-from enum import Enum
-from price import REGULAR, NEW_RELEASE, CHILDREN
+"""Module used for Movie class."""
+from dataclasses import dataclass
 
 
+@dataclass(frozen=True)
 class Movie:
     """
     A movie available for rent.
     """ 
-    def __init__(self, title):
+    def __init__(self, title, year, genre):
         # Initialize a new movie.
         self.title = title
+        self.year = year
+        self.genre = genre
+
+    def is_genre(self, other_genre: str):
+        """Give a boolean if the string match any movie's genre."""
+        return self.genre.lower_case() == other_genre.lower_case()
 
     def get_title(self):
+        """Give a title of a movie."""
         return self.title
-    
+
     def __str__(self):
-        return self.title
+        return f"{self.title}({self.year})"
