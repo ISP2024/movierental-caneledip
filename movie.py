75cf9c42629105560fd6fdb0fbca05f87e3a481d
Canele <winwin6d@hotmail.com>
2024-10-20 20:50:48 +0700

    create factory method MovieCatalog

--- movie.py
+++ movie.py
@@ -1,5 +1,7 @@
 """Module used for Movie class."""
 from dataclasses import dataclass
+import logging
+import csv
 
 
 @dataclass(frozen=True)
@@ -7,11 +9,11 @@ class Movie:
     """
     A movie available for rent.
     """ 
-    def __init__(self, title, year, genre):
+    def __init__(self, title, year, genres):
         # Initialize a new movie.
         self.title = title
         self.year = year
-        self.genre = genre
+        self.genres = genres
 
     def is_genre(self, other_genre: str):
         """Give a boolean if the string match any movie's genre."""
@@ -23,3 +25,46 @@ class Movie:
 
     def __str__(self):
         return f"{self.title}({self.year})"
+
+
+class MovieCatalog():
+    """A catalog of movies from a csv file."""
+    _instance = None
+
+    @classmethod
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(MovieCatalog, cls).__new__(cls)
+            cls._instance.movie_catalog = []
+            cls._instance.add_file_movies()
+        return cls._instance
+    
+    def __init__(self):
+        self.movie_catalog = []
+
+    def add_file_movies(self) -> None:
+        """Load movies from CSV and store them as Movie objects."""
+        try:
+            with open('movies.csv', 'r') as movie_file:
+                movie_csv = csv.reader(movie_file)
+                header = next(movie_csv, None) # Skip header
+
+                for row in movie_csv:
+                    try:
+                        title = row[1]
+                        year = int(row[2])
+                        genres = row[3].split("|")
+                        self.movie_catalog.append(Movie(title=title, year=year, genres=genres))
+                    except (ValueError, IndexError) as e:
+                        log = logging.getLogger()
+                        log.error(f"Error occured when processing row {row} - {e}.")
+        except FileNotFoundError:
+            log = logging.getLogger()
+            log.error("Movies CSV file not found.")
+
+    def get_movie(self, title: str, year: int = None) -> Movie:
+        """Return the first movie matching the title (and optional year)."""
+        for movie in self.movie_catalog:
+            if movie.title == title and (year is None or movie.year == year):
+                return movie
+        return None
---
+++ price.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+
+
+class PriceStrategy(ABC):
+    """Abstract class for rental pricing."""
+    # _instance = None
+
+    # @classmethod
+    # def __new__(cls):
+    #     if not cls._instance:
+    #         cls._instance = super(PriceStrategy, cls).__new__(cls)
+    #     return cls._instance
+
+    @abstractmethod
+    def get_price(self, days: int) -> float:
+        """The price for this movie rental."""
+        pass
+
+    @abstractmethod
+    def get_rental_points(self, days: int) -> int:
+        """The points earned from this movie rental."""
+        pass
+
+
+class RegularPrice(PriceStrategy):
+    """Rule for regular price strategy."""
+    def get_price(self, days: int):
+        """Regular price is 2 for 2 days, 1.5 for each days after."""
+        if days > 2:
+            return 2 + (days-2)*1.5
+        return 2
+
+    def get_rental_points(self, days: int):
+        """Regular movie give 1 point per rental."""
+        return 1
+
+
+class ChildrenPrice(PriceStrategy):
+    """Rule for children price strategy."""
+    def get_price(self, days: int):
+        """Children price is 1.5 for 3 days, 1.5 for each days after."""
+        if days > 3:
+            return 1.5 + (days-3)*1.5
+        return 1.5
+
+    def get_rental_points(self, days: int):
+        """Children movie give 1 point per rental."""
+        return 1
+
+
+class NewReleasePrice(PriceStrategy):
+    """Rule for new release price strategy."""
+    def get_price(self, days: int):
+        """New release price is 3 for each  days."""
+        return days*3
+
+    def get_rental_points(self, days):
+        """New release movie give 1 point per days of rental."""
+        return days
